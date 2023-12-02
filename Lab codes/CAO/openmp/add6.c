61e85aa502500024d26dc947ad12f62d95397d0e
Udaychandra <udaychandra1999@gmail.com>
2023-12-02 14:43:12 +0530

    initial create

---
+++ Lab codes/CAO/openmp/add6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<omp.h>
+
+void main()
+{
+    int a[5] = {1};
+    int b[5] = {1,2,3,4,5};
+    int c[5];
+    int tid;
+    #pragma omp parallel num_threads(6)
+    {
+        tid = omp_get_thread_num();
+        c[tid] = a[tid] + b[tid];
+        printf("c[%d] = %d\n",tid,c[tid]);
+    }
+}
