894f68ec58bb2c6f02061f7a1135df1a47ec2b91
Udaychandra <udaychandra1999@gmail.com>
2023-12-02 18:03:23 +0530

    changes commit

--- Lab codes/CAO/openmp/randomadd.c
+++ Lab codes/CAO/openmp/randomadd.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
-#include <omp.h>
 #include <stdlib.h>
-// This code is to add some random numbers to check the time taken in both sequential and parallel time execution
+#include <omp.h>
 
-int main()
-{
-    int a, b;
-    int sadd = 0, padd = 0;
-    printf("Sequential time execution:\n");
-    double st1, st2;
-    st1 = omp_get_wtime();
-    for (int i = 0; i < 1000000000; i++)
-    {
-        a = 1;
-        sadd += a;
+#define SIZE 1000
+
+int main() {
+    int i;
+    int sum_serial = 0, sum_parallel = 0;
+    int numbers[SIZE];
+
+    // Initialize array with random numbers
+    for (i = 0; i < SIZE; i++) {
+        numbers[i] = rand() % 100;
     }
-    st2 = omp_get_wtime();
-    printf("Serial time sum = %d and execution time %lf\n", sadd, st2 - st1);
-    printf("Parallel time execution:\n");
-    double pt1, pt2;
-    pt1 = omp_get_wtime();
-#pragma omp parallel for reduction(+ : padd)
-        for (int i = 0; i < 1000000000; i++)
-        {
-            a = 1;
-            padd += a;
+
+    // Serial computation
+    double start_time_serial = omp_get_wtime();
+    for (i = 0; i < SIZE; i++) {
+        sum_serial += numbers[i];
+    }
+    double end_time_serial = omp_get_wtime();
+
+    printf("Serial sum: %d\n", sum_serial);
+    printf("Serial time: %f seconds\n", end_time_serial - start_time_serial);
+
+    // Parallel computation using OpenMP
+    int num_threads;
+    double start_time_parallel = omp_get_wtime();
+    #pragma omp parallel
+    {
+        #pragma omp for reduction(+:sum_parallel)
+        for (i = 0; i < SIZE; i++) {
+            sum_parallel += numbers[i];
         }
-    pt2 = omp_get_wtime();
-    printf("Parallel time sum = %d and execution time %lf\n", padd, pt2 - pt1);
+
+        // Get the number of threads used
+        #pragma omp single
+        num_threads = omp_get_num_threads();
+    }
+    double end_time_parallel = omp_get_wtime();
+
+    printf("Parallel sum: %d\n", sum_parallel);
+    printf("Parallel time: %f seconds\n", end_time_parallel - start_time_parallel);
+    printf("Number of threads: %d\n", num_threads);
+
     return 0;
 }
