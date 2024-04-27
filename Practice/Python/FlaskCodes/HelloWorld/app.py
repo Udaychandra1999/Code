5f80624f00ea764ac88364de811ba40a6abdabf0
Udaychandra1999 <146862819+Udaychandra1999@users.noreply.github.com>
2024-04-27 18:42:48 +0530

    final changes

--- Practice/Python/FlaskCodes/HelloWorld/app.py
+++ Practice/Python/FlaskCodes/HelloWorld/app.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def index():
     greeting= "Hello world"
-    return render_template('index.html',greeting=greeting)
+    return render_template('index.html', greeting = greeting)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
