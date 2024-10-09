65253df299574b6cb3db4f68d4441f5786a0dc9b
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2024-10-09 14:45:38 +0530

    modified:   Hello.java

--- Practice/Java/Hello.java
+++ Practice/Java/Hello.java
@@ -1,7 +1,6 @@
 class Hello {
     public static void main(String[] args)
     {
-    // line comment describes hello world
         System.out.println("Hello world welcome to java programming");
     }
 }
